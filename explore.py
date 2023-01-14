b2b18c2d5a57b98830ef033bbf73f6c9a8e3faea
Cristina Lucin <cristinajlucin@gmail.com>
2023-01-14 11:03:17 -0600

    added wordcloud code to explore.py

--- explore.py
+++ explore.py
@@ -155,14 +155,16 @@ def get_top10_js(train):
     
     plt.show()
     
-#Get Java Wordcloud
+    
+############ WORDCLOUDS ###################
+
 def get_java_wordcloud():
     '''
     This function creates a wordcloud utilizing words from the Readme files of Minecraft
     GitHub Repositories. It utilizes a TXT file containing a string of words taken from
     Repositories that use the specified 'Java' language, and an image file located
     in the Repo to create the appropriate shape
-    ***YOU MUST HAVE THE PNG FILE AND TEXT FILE IN YOUR REPO WITH THE CORRECT PATHS***
+    ***YOU MUST HAVE THE PNG FILE AND TEXT FILE IN YOUR REPO WITH YOUR FILE PATHS***
     '''
     #Import TXT file of all JS words
     java_text = open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/all_java_readme.txt',
@@ -172,16 +174,110 @@ def get_java_wordcloud():
     # replace 0 with 255 inside the mask to ensure white background
     mask[mask == 0] = 255
     # Define Colors
-    colors = ['darkorange', 'steelblue']
+    colors = ['royalblue', 'cornflowerblue']
     custom_cmap = mcolors.ListedColormap(colors)
     #Make the wordcloud, generate the image
     wc = WordCloud(
-               mask = mask, background_color = "black",
-               max_words = 500, max_font_size = 500,
+               mask = mask, background_color = "gainsboro",
+               max_words = 1500, max_font_size = 500,
                random_state = 42, width = mask.shape[1],
-               colormap= custom_cmap, contour_color='darkorange', contour_width=1,
+               colormap= custom_cmap,
+               contour_color='orangered', contour_width=2,
                height = mask.shape[0])
     wc.generate(java_text)
+    plt.imshow(wc, interpolation="None")
+    plt.axis('off')
+    plt.show()
+    
+    
+def get_python_wordcloud():
+    '''
+    This function creates a wordcloud utilizing words from the Readme files of Minecraft
+    GitHub Repositories. It utilizes a TXT file containing a string of words taken from
+    Repositories that use the specified 'Python' language, and an image file located
+    in the Repo to create the appropriate shape
+    ***YOU MUST HAVE THE PNG FILE AND TEXT FILE IN YOUR REPO WITH YOUR FILE PATHS***
+    '''
+    #Import TXT file of all JS words
+    python_text = open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/all_python_readme.txt',
+            mode='r', encoding='utf-8').read()
+    #Import .png file of JS logo, create a Numpy array mask from the image
+    mask = np.array(Image.open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/green_python.png'))
+    # replace 0 with 255 inside the mask to ensure white background
+    mask[mask == 0] = 255
+    #Create Custom Colors
+    colors = ['#4B8BBE', '#306998', '#FFE873', '#FFD43B', '#646464']
+    custom_cmap = mcolors.ListedColormap(colors)
+    #Make the wordcloud, generate the image
+    wc = WordCloud(
+               mask = mask, background_color = "white",
+               max_words = 1000, max_font_size = 500,
+               random_state = 42, width = mask.shape[1],
+               colormap= custom_cmap,
+               contour_color='#306998', contour_width=2,
+               height = mask.shape[0])
+    wc.generate(python_text)
+    plt.imshow(wc, interpolation="None")
+    plt.axis('off')
+    plt.show()
+    
+
+def get_js_wordcloud():
+    '''
+    This function creates a wordcloud utilizing words from the Readme files of Minecraft
+    GitHub Repositories. It utilizes a TXT file containing a string of words taken from
+    Repositories that use the specified 'JavaScript' language, and an image file located
+    in the Repo to create the appropriate shape
+    ***YOU MUST HAVE THE PNG FILE AND TEXT FILE IN YOUR REPO WITH YOUR FILE PATHS***
+    '''
+    #Import TXT file of all JS words
+    js_text = open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/all_javascript_readme.txt',
+            mode='r', encoding='utf-8').read()
+    #Import .png file of JS logo, create a Numpy array mask from the image
+    mask = np.array(Image.open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/js.png'))
+    # replace 0 with 255 inside the mask to ensure white background
+    mask[mask == 0] = 255
+    # Define Colors
+    colors = ['black', '#323330', 'dimgray']
+    custom_cmap = mcolors.ListedColormap(colors)
+    #Make the wordcloud, generate the image
+    wc = WordCloud(
+               mask = mask, background_color = "#F0DB4F",
+               max_words = 1500, max_font_size = 500,
+               random_state = 42, width = mask.shape[1],
+               colormap = custom_cmap, contour_color='#F0DB4F', contour_width=1,
+               height = mask.shape[0])
+    wc.generate(js_text)
+    plt.imshow(wc, interpolation="None")
+    plt.axis('off')
+    plt.show()
+    
+    
+def get_sword_wordcloud():
+    '''
+    This function creates a wordcloud utilizing words from the Readme files of Minecraft
+    GitHub Repositories. It utilizes a TXT file containing a string of words taken from
+    Repositories that use the specified 'Other' language, and an image file located
+    in the Repo to create the appropriate shape
+    '''
+    #Import TXT file of all JS words
+    other_text = open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/all_other_readme.txt',
+            mode='r', encoding='utf-8').read()
+    #Import .png file of JS logo, create a Numpy array mask from the image
+    mask = np.array(Image.open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/sword.png'))
+    # replace 0 with 255 inside the mask to ensure white background
+    mask[mask == 0] = 255
+    # Define Colors
+    colors = ['#80c71f', '#5d7c15']
+    custom_cmap = mcolors.ListedColormap(colors)
+    #Make the wordcloud, generate the image
+    wc = WordCloud(
+               mask = mask, background_color = "#474f52",
+               max_words = 1500, max_font_size = 500,
+               random_state = 42, width = mask.shape[1],
+               colormap = custom_cmap, contour_color='#80c71f', contour_width=0.5,
+               height = mask.shape[0])
+    wc.generate(other_text)
     plt.imshow(wc, interpolation="None")
     plt.axis('off')
     plt.show()
