I changed a piece of code for each of the model functions and then did not correct for that change for the function which called the gradient booster test Sunday. It’s fixed now. I also changed the pathname for the files in “explore.” They were hardcoded to look for root/cristinalucin/something … /other folder/ CodeUp/MinecraftNLP/name of picture. I changed the name to be flexible to work for anyone who clones the project and attempts to run our notebook.

explore.py
@@ -167,10 +167,10 @@ def get_java_wordcloud():
     ***YOU MUST HAVE THE PNG FILE AND TEXT FILE IN YOUR REPO WITH YOUR FILE PATHS***
     '''
     #Import TXT file of all JS words
-    java_text = open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/all_java_readme.txt',
+    java_text = open(r'all_java_readme.txt',
             mode='r', encoding='utf-8').read()
     #Import .png file of JS logo, create a Numpy array mask from the image
-    mask = np.array(Image.open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/java.png'))
+    mask = np.array(Image.open(r'java.png'))
     # replace 0 with 255 inside the mask to ensure white background
     mask[mask == 0] = 255
     # Define Colors
@@ -199,10 +199,10 @@ def get_python_wordcloud():
     ***YOU MUST HAVE THE PNG FILE AND TEXT FILE IN YOUR REPO WITH YOUR FILE PATHS***
     '''
     #Import TXT file of all JS words
-    python_text = open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/all_python_readme.txt',
+    python_text = open(r'all_python_readme.txt',
             mode='r', encoding='utf-8').read()
     #Import .png file of JS logo, create a Numpy array mask from the image
-    mask = np.array(Image.open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/green_python.png'))
+    mask = np.array(Image.open(r'green_python.png'))
     # replace 0 with 255 inside the mask to ensure white background
     mask[mask == 0] = 255
     #Create Custom Colors
@@ -231,10 +231,10 @@ def get_js_wordcloud():
     ***YOU MUST HAVE THE PNG FILE AND TEXT FILE IN YOUR REPO WITH YOUR FILE PATHS***
     '''
     #Import TXT file of all JS words
-    js_text = open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/all_javascript_readme.txt',
+    js_text = open(r'all_javascript_readme.txt',
             mode='r', encoding='utf-8').read()
     #Import .png file of JS logo, create a Numpy array mask from the image
-    mask = np.array(Image.open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/js.png'))
+    mask = np.array(Image.open(r'js.png'))
     # replace 0 with 255 inside the mask to ensure white background
     mask[mask == 0] = 255
     # Define Colors
@@ -261,10 +261,10 @@ def get_sword_wordcloud():
     in the Repo to create the appropriate shape
     '''
     #Import TXT file of all JS words
-    other_text = open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/all_other_readme.txt',
+    other_text = open(r'all_other_readme.txt',
             mode='r', encoding='utf-8').read()
     #Import .png file of JS logo, create a Numpy array mask from the image
-    mask = np.array(Image.open(r'/Users/crislucin/codeup-data-science/MinecraftNLP/sword.png'))
+    mask = np.array(Image.open(r'sword.png'))
     # replace 0 with 255 inside the mask to ensure white background
     mask[mask == 0] = 255
     # Define Colors